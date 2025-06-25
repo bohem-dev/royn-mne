e773a9889488dff1a8c604c9fe19d0b6df4b808d
bohem-dev <p.press@protonmail.com>
2025-06-26 01:08:10 +0200

    tailwind finally working

---
+++ tailwindcss.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
